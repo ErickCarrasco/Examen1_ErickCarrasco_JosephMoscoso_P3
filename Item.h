8f62178d57af6f9eee8d10f917b82238fe1a320e
Erick Carrasco <erick.e.carrasco88@gmail.com>
2018-11-09 14:41:57 -0600

    Headers Item editado

--- Item.h
+++ Item.h
@@ -8,13 +8,38 @@ class Item{
 	private:
 		int lives;
 		int sizeBar;
+		int cantGolpes;
+		int proxX;
+		int proxY;
+		char tipo;
+		//char direction;
 	public:	
-		bool collision;
-		
-		void moveBar();
 
+		//Getters y setters
+		int getLives();
+		void setLives(int);
+
+		int getsizeBar();
+		void setSizeBar(int);
+
+		int getCantGolpes();
+		void setCantGolpes(int);
+
+		int getProxX();
+		void setProxX(int);
+	
+		int getProxY();
+		void setProxY(int);
+
+		char getTipo();
+		void setTipo(char);
+
+		//bool collision;
+		//void moveBar();
 		
-		
+		~Item();
+		//Destructor
+		//void start();
 
 
 };
