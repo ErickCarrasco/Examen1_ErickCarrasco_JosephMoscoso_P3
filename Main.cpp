d4dd9b1342bea8fe3284d6d1799b6f0398d31b60
Joseph Moscoso <josephmoscoso70@hotmail.com>
2018-11-09 14:38:28 -0600

    extrallendo data

--- Main.cpp
+++ Main.cpp
@@ -7,11 +7,29 @@ using std::endl;
 #include <ncurses.h>
 
 Item*** crearTablero();
+void llenar(Item***,int,int);
 
 int main(){
     Item*** tablero=NULL;
     tablero=crearTablero();
+    int desicion=0;
+    initscr();
+    printw("1)Nivel 1\n2)Nivel 2\n3)Nivel 3\nIngrese el número correspondiente: ");
+    //cin>>desicion;
+    refresh();
+    desicion=getch();
+    while(desicion<0||desicion>3){
+
+    }
+    
+    //getch();
+    endwin();
+    void llenar(tablero, 10, desicion); 
     return 0;
+}
+
+void llenar(Item*** tablero, int size, int desicion){
+
 }
 
 Item*** crearTablero(){
